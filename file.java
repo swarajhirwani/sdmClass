c38782a8029c869ef63058601bf6b8edfd2b211f
Swaraj <swarajhirwani@gmail.com>
2022-12-26 18:24:38 +0530

    restored to file.java

---
+++ file.java
@@ -0,0 +1,10 @@
+class file {
+    public static void main(String[] args) {
+        int x = 3;
+        int y = 7;
+        int z = x + y;
+        System.out.println("value of x: "+x);
+        System.out.println("value of y: "+y);
+        System.out.println("value of z: "+z);
+    }
+}
